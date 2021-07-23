1d73dac7c35512b8d58ad5684f91744654128283
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-07-23 17:04:00 -0300

    Validaçao dos campos NAME,EMAIL E PASSWORD funcionando!

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,52 @@
 import User from '../models/User';
 
 
-class UserController{
-    async store(req, res){
-        const user = await User.create(req.body);
-        return res.json(user);
+class UserController {
+    async store(req, res) {
+        const emailExiste = await User.findOne({ email: req.body.email });
+        if (emailExiste) {
+            return res.status(400).json({
+                error: true,
+                code: 102,
+                message: "Error: email já cadastrado!"
+            });
+        }
+        if(!req.body.name || typeof req.body.name == undefined || req.body.name == null){
+            return res.status(400).json({
+                error: true,
+                code: 103,
+                message: "Error: Por favor, preencher o campo nome do Pet!"
+            });
+        }
+        if(!req.body.email || typeof req.body.email == undefined || req.body.email == null){
+            return res.status(400).json({
+                error: true,
+                code: 104,
+                message: "Error: Por favor, preencher o campo email do tutor!"
+            });
+        }
+        if(!req.body.password || typeof req.body.password == undefined || req.body.password == null){
+            return res.status(400).json({
+                error: true,
+                code: 105,
+                message: "Error: Por favor, preencher o campo senha!"
+            });
+        }
+
+        const user = await User.create(req.body, (err) => {
+            if (err) return res.status(400).json({
+                error: true,
+                code: 101,
+                message: "Error: Não foi cadastrados com sucesso"
+            });
+
+            return res.status(200).json({
+                error: false,
+                message: "Pet cadastrado com sucesso!",
+                dados: user
+            })
+        });
+
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -11,18 +11,4 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 
-/*routes.get('/', async (req,res) =>{
-    await User.create({
-        nome: 'Stitch4',
-        email: 'dog@dog.com',
-        password:'123456'
-    },function (err, small){
-        if(err) return res.status(400).json({error: "erro: não consegui cadastrar"});
-        return res.status(200).json({error: "Usuario foi cadastrado."});
-    });
-   
-})*/
-
-//module.exports = routes;
-
 export default routes;
