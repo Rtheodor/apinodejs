f38fb99ad3cc1d8ab4f28d9272722acf12ac036f
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-07-26 18:19:24 -0300

    Validaçao com dependencia Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,31 @@
-import { restart } from 'nodemon';
+import * as Yup from 'yup';
 import User from '../models/User';
-
 
 class UserController {
     async store(req, res) {
         
+        //validando com Yup
+        const schema = Yup.object().shape({
+            emailTutor: Yup.string()
+            .email()
+            .required(),
+            namePet: Yup.string()
+            .required(),
+            rga: Yup.number()
+            .required(),
+            raca: Yup.string(),
+            sexPet: Yup.string()
+             
+        });
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({
+                error: true,
+                code: 103,
+                message: "Error: Dados invalidos!"
+            });
+        }
+
         const emailExiste = await User.findOne({ emailTutor: req.body.emailTutor });
         if (emailExiste) {
             return res.status(400).json({
@@ -13,7 +34,24 @@ class UserController {
                 message: "Error: email do Tutor já cadastrado!"
             });
         }
-        if(!req.body.namePet || typeof req.body.namePet == undefined || req.body.namePet == null){
+
+        const user = await User.create(req.body, (err) => {
+            if (err) return res.status(400).json({
+                error: true,
+                code: 101,
+                message: "Error: O Pet Não foi cadastrados com sucesso"
+            });
+
+            return res.status(200).json({
+                error: false,
+                message: "Pet cadastrado com sucesso!",
+                dados: user
+            })
+        });
+
+        //abaixo esta a forma de validação sem o yup
+
+        /*if(!req.body.namePet || typeof req.body.namePet == undefined || req.body.namePet == null){
             return res.status(400).json({
                 error: true,
                 code: 103,
@@ -26,7 +64,7 @@ class UserController {
                 code: 104,
                 message: "Error: Por favor, preencher o campo email do tutor!"
             });
-        }
+        }*/
 
         
         /*const rgaExiste = await User.findOne({rgaExiste:req.body.rgaExiste});
@@ -43,7 +81,7 @@ class UserController {
                 code:106,
                 message:"Erro:Por favor preencher o campo rga!"
             });
-        }*/
+        }
 
 
         
@@ -55,19 +93,7 @@ class UserController {
             });
         }*/
 
-        const user = await User.create(req.body, (err) => {
-            if (err) return res.status(400).json({
-                error: true,
-                code: 101,
-                message: "Error: O Pet Não foi cadastrados com sucesso"
-            });
-
-            return res.status(200).json({
-                error: false,
-                message: "Pet cadastrado com sucesso!",
-                dados: user
-            })
-        });
+        
 
         
 
