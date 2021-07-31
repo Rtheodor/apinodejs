cee74e41efc5b20988bea58debf7950ad8adfce3
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-07-31 16:20:25 -0300

    Upload das imagens funcionando

---
+++ src/app/controllers/PerfilController.js
@@ -0,0 +1,85 @@
+import * as Yup from 'yup';
+import bcrypt from 'bcryptjs';
+import User from '../models/User';
+
+class PerfilController{
+    async show(req, res){
+        User.findOne({_id: req.userId }, '_id emailTutor namePet  createdAt updatedAt').then((user) => {
+            return res.json({
+                error: false,
+                user: user
+            });
+        }).catch((erro) => {
+            return res.status(400).json({
+                error: true,
+                code: 115,
+                message: "Erro: Pet não encontrado!"
+            });
+        });
+    };
+    async update(req,res){
+        const schema = Yup.object().shape({
+            emailTutor: Yup.string()
+            .email(),
+            password: Yup.string()
+            .min(6),
+            namePet: Yup.string(),
+            rga: Yup.number(),
+            raca: Yup.string(),
+            sexPet: Yup.string()
+        });
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({
+                error: true,
+                code:108,
+                message: "Erro: Dados do formulario invalido"
+            });
+        };
+
+        const { emailTutor } = req.body;
+
+        const usuarioExiste = await User.findOne({_id: req.userId});
+
+        if(!usuarioExiste){
+            return res.status(400).json({
+                error:true,
+                code:109,
+                message: "Erro: Usuário nao encontrado!"
+            })
+        }
+
+        if(emailTutor != usuarioExiste.emailTutor){
+            const emailExiste = await User.findOne({emailTutor})
+            if (emailExiste){
+                return res.status(400).json({
+                    error:true,
+                    code:110,
+                    message: "Erro: este e-mail ja esta cadastrado!"
+                });
+            };
+        };
+        var dados = req.body;
+        if(dados.password){
+            dados.password = await bcrypt.hash(dados.password, 8)
+        };
+
+        await User.updateOne({_id: dados._id}, dados, (err)=>{
+            if(err) return res.status(400).json({
+                error: true,
+                code:111,
+                message: "Erro: Usuário nao foi editado com sucesso! "
+            });
+            
+            return res.json({
+                error:false,
+                message: "Usuário editado com sucesso e muito frio."
+            })
+        });
+        
+        
+
+    }
+};
+
+export default new PerfilController();
--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,29 @@
 import { Router } from 'express';
 import mongoose from 'mongoose';
 import User from './app/models/User';
+import multer from 'multer';
+import multerUpImgUsers from './app/middlewares/uploadImgUser';
 
 import UserController from './app/controllers/UserController';
 import loginController from './app/controllers/loginController';
+import PerfilController from './app/controllers/PerfilController';
+import PerfilImagemController from './app/controllers/PerfilImagemController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const uploadImgUser = multer(multerUpImgUsers);
+
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
 routes.post('/users', UserController.store);
-routes.put('/users', UserController.update);
+routes.put('/users', authMiddleware, UserController.update);
 routes.delete('/users/:id',authMiddleware, UserController.delete);
+
+routes.get('/perfil', authMiddleware, PerfilController.show);
+routes.put('/perfil', authMiddleware, PerfilController.update);
+routes.put('/perfil-img',authMiddleware, uploadImgUser.single('file'), PerfilImagemController.update);
+
 routes.post('/login', loginController.store);
 
 export default routes;
